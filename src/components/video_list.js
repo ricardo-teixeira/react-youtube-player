c78a10fef6ee3da8fb402ceb052ef67ccd4e8bc1
Ricardo Teixeira <Ricardo Teixeira>
2017-02-09 00:02:39 -0200

    First commit

---
+++ src/components/video_list.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+
+  const videosItems = props.videos.map((video)=> {
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag} video={video} />
+    );
+  });
+
+  return (
+    <ul className="col-lg-4 list-group">
+      {videosItems}
+    </ul>
+  );
+};
+
+export default VideoList;
